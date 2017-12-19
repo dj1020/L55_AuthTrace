10a9e6aba77afe51eac63d9e74f63d08e29a5d47
Ken Lin <kenlin@kkbox.com>
2017-12-19 16:54:21 +0800

    蓋掉 credentials，可以 username 或 email 登入了

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -40,5 +41,17 @@ class LoginController extends Controller
     public function username()
     {
         return 'username';
+    }
+
+    protected function credentials(Request $request)
+    {
+        $field = filter_var($request->get($this->username()), FILTER_VALIDATE_EMAIL)
+            ? 'email'
+            : $this->username();
+
+        return [
+            $field     => $request->get($this->username()),
+            'password' => $request->get('password'),
+        ];
     }
 }
